ba0304d5a57ae7c8f923aaf1c9d8c53c9825f55d
Fang Yuan <yuanfang@seattleu.edu>
2019-06-09 15:40:33 -0700

    Added ReportViewModel_LogViewModel_Get_Default_Should_Pass() test case.

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -19,5 +19,18 @@ namespace UnitTests.Models
             //Assert
             Assert.IsNotNull(result);
         }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            //Arrange
+            var myTest = new ReportViewModel();
+
+            //Act
+            var result = myTest.LogViewModel;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
