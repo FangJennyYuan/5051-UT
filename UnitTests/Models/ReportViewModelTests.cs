8b6fe031418d1f8e75b983764f08c29b428f207a
Fang Yuan <yuanfang@seattleu.edu>
2019-06-09 15:37:21 -0700

    Revert "Added ReportViewModel_Set_LogViewModel_Should_Pass() test case."
    This reverts commit d11de834ffea2b09e6a68c604772041f4dd7418d.

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HW1c.Models;
-using System.Collections.Generic;
 
 namespace UnitTests.Models
 {
@@ -19,37 +18,6 @@ namespace UnitTests.Models
 
             //Assert
             Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void ReportViewModel_Get_LogViewModel_Should_Pass()
-        {
-            //Arrange
-            var myTest = new ReportViewModel();
-
-            //Act
-            var result = myTest.LogViewModel;
-
-            //Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void ReportViewModel_Set_LogViewModel_Should_Pass()
-        {
-            //Arrange
-            var myTest = new ReportViewModel();
-            var myViewModel = new LogViewModel();
-            var myList = new List<LogModel>();
-            myList.Add(new LogModel { PhoneID = "Phone" });
-            myViewModel.LogList = myList;
-
-            //Act
-            myTest.LogViewModel = myViewModel;
-            var result = myTest.LogViewModel;
-
-            //Assert
-            Assert.AreEqual("Phone", result.LogList[0].PhoneID);
         }
     }
 }
